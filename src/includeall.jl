0c55cf4cbf6e33d7616ffcf865fa250ac66c2695
William Chen <chenwilliamy77@gmail.com>
2020-06-08 16:49:00 -0400

    Add includeall for Julia files.

---
+++ src/includeall.jl
@@ -0,0 +1,8 @@
+using DSGE, ModelConstructors, OrderedCollections, Random
+
+# julia_models/JPT
+include("julia_models/JPT/jpt.jl")
+include("julia_models/JPT/eqcond.jl")
+include("julia_models/JPT/measurement.jl")
+include("julia_models/JPT/observables.jl")
+include("julia_models/JPT/subspecs.jl")
